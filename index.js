f25b3b1974b16411e006278f52ab6627bd53fe19
Ariel Hicks <hicks.ariel@gmail.com>
2018-12-15 21:47:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  var object2 = Object.assign = ({}, object)
+  var Object2 = Object.assign = ({}, object)
   delete object[key]
 }
