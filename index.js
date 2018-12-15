6d358c3505c9366011e8dc322905a975303c1597
Ariel Hicks <hicks.ariel@gmail.com>
2018-12-15 20:51:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,5 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  recipes.prop2 = 2
-  return recipes
+  var object = key: value
 }
